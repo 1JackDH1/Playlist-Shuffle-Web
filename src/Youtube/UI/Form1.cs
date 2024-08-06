5a8508268b9f56b644e3cdb4ab8196e31bce08d7
1JackDH1 <jackdhowarth@hotmail.com>
2024-08-06 23:25:56 +0100

    Experiment with deprecated WebBrowser control - insertion of HTML (without JS) files and standalone Youtube videos

--- src/Youtube/UI/Form1.cs
+++ src/Youtube/UI/Form1.cs
@@ -5,5 +5,23 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        Load += Form1_loader;
+    }
+
+    private void Form1_loader(object? sender, EventArgs e)
+    {
+        // WebBrowser hosts IExplorer, now deprecated
+        // Replaced with Edge and WebView2
+
+        WebBrowser browser_form1 = new WebBrowser();
+        browser_form1.Dock = DockStyle.Fill;
+        browser_form1.Width = this.Width;
+        browser_form1.Height = this.Height;
+        browser_form1.ScrollBarsEnabled = true;
+        browser_form1.Visible = true;
+        
+        browser_form1.Navigate("https://youtu.be/SB-qEYVdvXA?si=3iVakbcyGV1fHje1");
+
+        this.Controls.Add(browser_form1);
     }
 }
--- src/Youtube/scripts/main.py
+++ src/Youtube/scripts/main.py
@@ -5,11 +5,12 @@ import playlistitems
 from shuffle import random_shuffle
 
 def main():
+    ''' Main function, consolidation of other Python scripts. '''
     playlist_ids = playlist.get_playlist()
     print("All playlists in account:")
     for list_id in playlist_ids:
         print(list_id + '\n')
-    
+
     video_id_list = playlistitems.get_playlist_items()
     shuffled_list = random_shuffle(video_id_list)
     print(len(shuffled_list))
